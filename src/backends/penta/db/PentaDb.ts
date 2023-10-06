f1022defe956af482fe33285f3a1a13826020de9
Will Hunt <will@half-shot.uk>
2023-10-06 12:12:39 +0100

    Don't expect penta config values to exist at top level

--- src/backends/penta/db/PentaDb.ts
+++ src/backends/penta/db/PentaDb.ts
@@ -22,13 +22,12 @@ import { objectFastClone } from "../../../utils";
 import { IDbTalk, IRawDbTalk } from "./DbTalk";
 import { IPerson, Role } from "../../../models/schedule";
 
-const PEOPLE_SELECT = "SELECT event_id::text, person_id::text, event_role::text, name::text, email::text, matrix_id::text, conference_room::text, remark::text FROM " + config.conference.schedule.database?.tblPeople;
-const NONEVENT_PEOPLE_SELECT = "SELECT DISTINCT 'ignore' AS event_id, person_id::text, event_role::text, name::text, email::text, matrix_id::text, conference_room::text FROM " + config.conference.schedule.database?.tblPeople;
-
 const START_QUERY = "start_datetime AT TIME ZONE $1 AT TIME ZONE 'UTC'";
 const QA_START_QUERY = "(start_datetime + presentation_length) AT TIME ZONE $1 AT TIME ZONE 'UTC'";
 const END_QUERY = "(start_datetime + duration) AT TIME ZONE $1 AT TIME ZONE 'UTC'";
-const SCHEDULE_SELECT = `SELECT DISTINCT event_id::text, conference_room::text, EXTRACT(EPOCH FROM ${START_QUERY}) * 1000 AS start_datetime, EXTRACT(EPOCH FROM duration) AS duration_seconds, EXTRACT(EPOCH FROM presentation_length) AS presentation_length_seconds, EXTRACT(EPOCH FROM ${END_QUERY}) * 1000 AS end_datetime, EXTRACT(EPOCH FROM ${QA_START_QUERY}) * 1000 AS qa_start_datetime, prerecorded FROM ` + config.conference.schedule.database?.tblSchedule;
+const PEOPLE_SELECT = "SELECT event_id::text, person_id::text, event_role::text, name::text, email::text, matrix_id::text, conference_room::text, remark::text FROM";
+const NONEVENT_PEOPLE_SELECT = "SELECT DISTINCT 'ignore' AS event_id, person_id::text, event_role::text, name::text, email::text, matrix_id::text, conference_room::text FROM";
+const SCHEDULE_SELECT = `SELECT DISTINCT event_id::text, conference_room::text, EXTRACT(EPOCH FROM ${START_QUERY}) * 1000 AS start_datetime, EXTRACT(EPOCH FROM duration) AS duration_seconds, EXTRACT(EPOCH FROM presentation_length) AS presentation_length_seconds, EXTRACT(EPOCH FROM ${END_QUERY}) * 1000 AS end_datetime, EXTRACT(EPOCH FROM ${QA_START_QUERY}) * 1000 AS qa_start_datetime, prerecorded FROM`;
 
 export class PentaDb {
     private client: Pool;
@@ -70,36 +69,36 @@ export class PentaDb {
     public async findPeopleWithId(personId: string): Promise<IPerson[]> {
         const numericPersonId = Number(personId);
         if (Number.isSafeInteger(numericPersonId)) {
-            const result = await this.client.query<IDbPerson>(`${PEOPLE_SELECT} WHERE person_id = $1 OR person_id = $2`, [personId, numericPersonId]);
+            const result = await this.client.query<IDbPerson>(`${PEOPLE_SELECT} ${config.conference.schedule.database?.tblPeople} WHERE person_id = $1 OR person_id = $2`, [personId, numericPersonId]);
             return this.sanitizeRecords(result.rows).map(dbPersonToPerson);
         } else {
-            const result = await this.client.query<IDbPerson>(`${PEOPLE_SELECT} WHERE person_id = $1`, [personId]);
+            const result = await this.client.query<IDbPerson>(`${PEOPLE_SELECT} ${config.conference.schedule.database?.tblPeople} WHERE person_id = $1`, [personId]);
             return this.sanitizeRecords(result.rows).map(dbPersonToPerson);
         }
     }
 
     public async findAllPeople(): Promise<IPerson[]> {
-        const result = await this.client.query<IDbPerson>(`${PEOPLE_SELECT}`);
+        const result = await this.client.query<IDbPerson>(`${PEOPLE_SELECT} ${config.conference.schedule.database?.tblPeople}`);
         return this.sanitizeRecords(result.rows).map(dbPersonToPerson);
     }
 
     public async findAllPeopleForAuditorium(auditoriumId: string): Promise<IPerson[]> {
-        const result = await this.client.query<IDbPerson>(`${NONEVENT_PEOPLE_SELECT} WHERE conference_room = $1`, [auditoriumId]);
+        const result = await this.client.query<IDbPerson>(`${NONEVENT_PEOPLE_SELECT} ${config.conference.schedule.database?.tblPeople} ${config.conference.schedule.database?.tblPeople} WHERE conference_room = $1`, [auditoriumId]);
         return this.sanitizeRecords(result.rows).map(dbPersonToPerson);
     }
 
     public async findAllPeopleForTalk(talkId: string): Promise<IPerson[]> {
-        const result = await this.client.query<IDbPerson>(`${PEOPLE_SELECT} WHERE event_id = $1`, [talkId]);
+        const result = await this.client.query<IDbPerson>(`${PEOPLE_SELECT} ${config.conference.schedule.database?.tblPeople} WHERE event_id = $1`, [talkId]);
         return this.sanitizeRecords(result.rows).map(dbPersonToPerson);
     }
 
     public async findAllPeopleWithRole(role: Role): Promise<IPerson[]> {
-        const result = await this.client.query<IDbPerson>(`${PEOPLE_SELECT} WHERE event_role = $1`, [role]);
+        const result = await this.client.query<IDbPerson>(`${PEOPLE_SELECT} ${config.conference.schedule.database?.tblPeople} WHERE event_role = $1`, [role]);
         return this.sanitizeRecords(result.rows).map(dbPersonToPerson);
     }
 
     public async findAllPeopleWithRemark(remark: string): Promise<IPerson[]> {
-        const result = await this.client.query<IDbPerson>(`${PEOPLE_SELECT} WHERE remark = $1`, [remark]);
+        const result = await this.client.query<IDbPerson>(`${PEOPLE_SELECT} ${config.conference.schedule.database?.tblPeople} WHERE remark = $1`, [remark]);
         return this.sanitizeRecords(result.rows).map(dbPersonToPerson);
     }
 
@@ -148,7 +147,7 @@ export class PentaDb {
      */
     public async getTalk(talkId: string): Promise<IDbTalk | null> {
         const result = await this.client.query(
-            `${SCHEDULE_SELECT} WHERE event_id::text = $2`,
+            `${SCHEDULE_SELECT} ${config.conference.schedule.database?.tblSchedule} WHERE event_id::text = $2`,
             [config.conference.timezone, talkId]);
         return result.rowCount > 0 ? this.postprocessDbTalk(result.rows[0]) : null;
     }
@@ -156,7 +155,7 @@ export class PentaDb {
     private async getTalksWithin(timeQuery: string, inNextMinutes: number, minBefore: number): Promise<IDbTalk[]> {
         const now = "NOW() AT TIME ZONE 'UTC'";
         const result = await this.client.query(
-            `${SCHEDULE_SELECT} WHERE ${timeQuery} >= (${now} - MAKE_INTERVAL(mins => $2)) AND ${timeQuery} <= (${now} + MAKE_INTERVAL(mins => $3))`,
+            `${SCHEDULE_SELECT} ${config.conference.schedule.database?.tblSchedule} WHERE ${timeQuery} >= (${now} - MAKE_INTERVAL(mins => $2)) AND ${timeQuery} <= (${now} + MAKE_INTERVAL(mins => $3))`,
             [config.conference.timezone, minBefore, inNextMinutes]);
         return this.postprocessDbTalks(result.rows);
     }
